2b40d52b14e73886a1881515f7cdeea4ea41d0c7
shahrukhx01 <sk28671@gmail.com>
2021-08-17 10:27:01 +0200

    add experinets dirs

---
+++ schema_aware_text2sql/deploy_hf.py
@@ -0,0 +1,19 @@
+## !apt-get install git-lfs
+## git config --global user.email "sk28671@gmail.com"
+from transformers import (
+    CONFIG_MAPPING,
+    AdamW,
+    AutoConfig,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    DataCollatorForSeq2Seq,
+    SchedulerType,
+    get_scheduler,
+    set_seed,
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained( "./output/model_2" )
+model.push_to_hub("schema-aware-distilbart-cnn-12-6-text2sql")
+
+tokenizer = AutoTokenizer.from_pretrained( "./output/model_2" )
+tokenizer.push_to_hub("schema-aware-distilbart-cnn-12-6-text2sql")
